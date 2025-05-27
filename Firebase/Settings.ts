1498397f7abe01d92c0ad6a106a515e4292d17ed
NonsoCoding <Dragoncrocodile1#>
2025-05-27 09:34:42 +0100

    NonsoLatest

--- Firebase/Settings.ts
+++ Firebase/Settings.ts
@@ -4,26 +4,18 @@ import { getAuth } from 'firebase/auth';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { getFirestore } from 'firebase/firestore'
-import {
-  FIREBASE_API_KEY,
-  FIREBASE_APP_ID,
-  FIREBASE_AUTH_DOMAIN,
-  FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUCKET,
-} from '@env';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID
+  apiKey: "AIzaSyAbB6rRODB0txennMzTHWwEibZPjJVoEQM",
+  authDomain: "tactical-pt.firebaseapp.com",
+  projectId: "tactical-pt",
+  storageBucket: "tactical-pt.firebasestorage.app",
+  messagingSenderId: "565483164663",
+  appId: "1:565483164663:web:2074b0d60e5a47f59a54f2"
 };
 
 // Initialize Firebase
